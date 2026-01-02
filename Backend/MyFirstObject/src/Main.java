3b36909725395b844013c80af8cd181734954fb3
Kahen T <bl0ckthings@proton.me>
2026-01-02 14:14:38 +0100

    OOP learn

--- Backend/MyFirstObject/src/Main.java
+++ Backend/MyFirstObject/src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import model.Patient;
+import model.Vehicle;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Patient patient = new Patient();
+        patient.setFirstName("Kolu");
+        System.out.println(patient.getFirstName());
+        Patient patient1 = new Patient();
+        patient1.setFirstName("Clara");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Vehicle car = new Vehicle("Audi","RS6",2018, true);
+        .println(car.start());
     }
 }
